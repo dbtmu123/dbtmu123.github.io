a857f6f03d112d4c8d487191886d26dd053819d7
dbtmu123 <dbhagaria@torontomu.ca>
2024-11-06 05:45:05 -0500

    fsdg

--- site/script.js
+++ site/script.js
@@ -7,3 +7,15 @@ function enlargeImage() {
     var img = document.getElementById("placeholderimage");
     img.style.width = "500px";
 }
+
+function showWelcomeMessage() {
+    var nameInput = document.getElementById("nameInput").value;
+    if (nameInput) {
+        document.getElementById("welcomeMessage").innerHTML = "Hello, " + nameInput + "! Welcome to my portfolio site.";
+        setTimeout(function() {
+            window.location.href = 'portfolio.html';
+        }, 2000);
+    } else {
+        document.getElementById("welcomeMessage").innerHTML = "Please enter your name.";
+    }
+}
